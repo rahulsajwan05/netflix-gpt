a6380987f2d1494dff89e6012835749a045a72ba
RahulSajwan <r.sajwan@novelvox.com>
2025-07-14 18:23:04 +0530

    adding user login and logout

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
-import Login from './Login'
-import Browse from './Browse'
-import { createBrowserRouter } from 'react-router-dom'
-import { RouterProvider } from 'react-router-dom'
+import React, { useEffect } from "react";
+import Login from "./Login";
+import Browse from "./Browse";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { addUser, removeUser } from "../utils/userSlice";
 const Body = () => {
-
-  const appRouter =  createBrowserRouter([
+  const dispatch = useDispatch();
+  const appRouter = createBrowserRouter([
     {
-        path: "/",
-        element: <Login/>
+      path: "/",
+      element: <Login />,
     },
     {
-        path: "/browse",
-        element: <Browse/>
-    }
-  ])  
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        const userData = {
+          uid: uid,
+          email: email,
+          displayName: displayName || "Guest",
+        };
+        dispatch(addUser({userData}));
+      } else {
+        dispatch(removeUser());
+      }
+    });
+  }, []);
+
   return (
     <div>
       <RouterProvider router={appRouter} />
-    </div> 
-  )
-}
+    </div>
+  );
+};
 
-export default Body
+export default Body;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,72 +1,130 @@
-import { useState , useRef } from "react";
+import { useState, useRef } from "react";
 import Header from "./Header";
-import { checkValidData , checkValidName, checkValidPassword } from "../utils/checkValidData";
+import {
+  checkValidData,
+  checkValidName,
+  checkValidPassword,
+} from "../utils/checkValidData";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
   const [isSingInForm, setIsSignInForm] = useState(true);
-  const [emailEM, setEmailEM] = useState('');
-  const [passEM, setPassEM] = useState('');
-  const [nameEM, setnameEM] = useState('');
+  const [emailEM, setEmailEM] = useState("");
+  const [passEM, setPassEM] = useState("");
+  const [nameEM, setnameEM] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  
   const email = useRef(null);
   const password = useRef(null);
   const username = useRef(null);
-  
+
   const toggleSignInForm = () => {
     resetForm();
     setIsSignInForm(!isSingInForm);
   };
-  const HandleSignIn = () =>{
-
-    const EmailErrormessage = checkValidData(email.current.value)
-    const passwordErrorMessage = checkValidPassword(password.current.value)
+  const HandleSignIn = () => {
+    const EmailErrormessage = checkValidData(email.current.value);
+    const passwordErrorMessage = checkValidPassword(password.current.value);
     setEmailEM(EmailErrormessage);
     setPassEM(passwordErrorMessage);
-    let NameErrorMessage = ''
-    if(!isSingInForm) {
-      NameErrorMessage =checkValidName(username?.current?.value)
+    let NameErrorMessage = "";
+    if (!isSingInForm) {
+      NameErrorMessage = checkValidName(username?.current?.value);
       setnameEM(NameErrorMessage);
     }
-    
-    if(EmailErrormessage || passwordErrorMessage || (!isSingInForm && NameErrorMessage)) {
+
+    if (
+      EmailErrormessage ||
+      passwordErrorMessage ||
+      (!isSingInForm && NameErrorMessage)
+    ) {
       return;
     }
-  }
 
-  const resetForm = () =>{
-    email.current.value = '';
-    password.current.value = '';
-    setEmailEM('');
-    setPassEM('');
-    if(!isSingInForm){
-      username.current.value = '';
-      setnameEM('');
+    if (!isSingInForm) {
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log("User signed up:", user);
+          navigate("/browse");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log("Error signing up:", errorCode, errorMessage);
+          setErrorMessage(`${errorCode}  ${errorMessage}`);
+        });
+    } else {
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+          navigate("/browse");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(`${errorCode}  ${errorMessage}`);
+        });
     }
-  }
+  };
+
+  const resetForm = () => {
+    email.current.value = "";
+    password.current.value = "";
+    setEmailEM("");
+    setPassEM("");
+    if (!isSingInForm) {
+      username.current.value = "";
+      setnameEM("");
+    }
+  };
 
   return (
     <div className="">
       <Header />
+
       <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_large.jpg"
           alt="background"
         />
       </div>
+
+
       <div className="absolute mx-auto my-36 right-0 left-0 w-3/12 bg-black bg-opacity-70 text-white rounded-lg p-8">
         <form onSubmit={(e) => e.preventDefault()}>
+          
           <h1 className="font-bold text-xl mb-4">
             {isSingInForm ? "Sign In" : "Sign Up"}
           </h1>
+
           <div className="flex flex-col">
-            {!isSingInForm && <input
-              ref={username}
-              type="text"
-              placeholder="Name"
-              className="p-2 my-2 bg-gray-700"
-            />}
+            {!isSingInForm && (
+              <input
+                ref={username}
+                type="text"
+                placeholder="Name"
+                className="p-2 my-2 bg-gray-700"
+              />
+            )}
             <p className="text-red-500 font-bold">{!isSingInForm && nameEM}</p>
+
+
             <input
               ref={email}
               type="text"
@@ -74,6 +132,8 @@ const Login = () => {
               className="p-2 my-2 bg-gray-700"
             />
             <p className="text-red-500 font-bold">{emailEM}</p>
+
+
             <input
               ref={password}
               type="password"
@@ -81,6 +141,9 @@ const Login = () => {
               className="p-2 my-2 bg-gray-700"
             />
             <p className="text-red-500 font-bold">{passEM}</p>
+            <p className="text-red-500 font-bold">{errorMessage}</p>
+
+
             <button
               type="submit"
               className="p-2 my-6 bg-red-600 rounded-lg"
@@ -88,11 +151,15 @@ const Login = () => {
             >
               {isSingInForm ? "Sign In" : "Sign Up"}
             </button>
+
+
             <p onClick={toggleSignInForm} className="cursor-pointer">
               {!isSingInForm
                 ? "Already registered ? Sign In now"
                 : "New to Netflix ? Sing Up"}
             </p>
+
+
           </div>
         </form>
       </div>
